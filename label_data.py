e0d9eb3931f1625f87a76bf4c904c657d778d540
Erik Remington Lee <anathema8808@yahoo.com>
2020-05-31 13:11:20 -0400

    created labeled data for quick and full

--- label_data.py
+++ label_data.py
@@ -7,17 +7,21 @@ from shutil import copyfile
 parent_path = "FERG_DB_256"
 
 def main():
+    Label().delete_files("emotions")
     Label().label_data(parent_path)
 
 class Label():
-    def __init__(self):
-        self.anger = "emotions/anger/"
-        self.disgust = "emotions/disgust/"
-        self.fear = "emotions/fear/"
-        self.joy = "emotions/joy/"
-        self.neutral = "emotions/neutral/"
-        self.sadness = "emotions/sadness/"
-        self.surprise = "emotions/surprise/"
+    def __init__(self, train="emotions/train/", test="emotions/test/"):
+        self.train = train
+        self.test = test
+        self.anger = "anger"
+        self.disgust = "disgust"
+        self.fear = "fear"
+        self.joy = "joy"
+        self.neutral = "neutral"
+        self.sadness = "sadness"
+        self.surprise = "surprise"
+        self.train_test_split = 0.9
 
     def label_data(self, parent_path):
         list_dir = os.listdir(parent_path)
@@ -28,30 +32,52 @@ class Label():
                 for x in os.listdir(p):
                     # Example: "FERG_DB_256/aia/anger"   
                     final_dir = os.path.join(p,x)
-                    if "anger" in x and os.path.isdir(final_dir):
+                    if self.anger in x and os.path.isdir(final_dir):
                         self.copy_files(final_dir, self.anger)
-                    elif "disgust" in x and os.path.isdir(final_dir):
+                    elif self.disgust in x and os.path.isdir(final_dir):
                         self.copy_files(final_dir, self.disgust)
-                    elif "fear" in x and os.path.isdir(final_dir):
+                    elif self.fear in x and os.path.isdir(final_dir):
                         self.copy_files(final_dir, self.fear)
-                    elif "joy" in x and os.path.isdir(final_dir):
+                    elif self.joy in x and os.path.isdir(final_dir):
                         self.copy_files(final_dir, self.joy)
-                    elif "neutral" in x and os.path.isdir(final_dir):
+                    elif self.neutral in x and os.path.isdir(final_dir):
                         self.copy_files(final_dir, self.neutral)
-                    elif "sadness" in x and os.path.isdir(final_dir):
+                    elif self.sadness in x and os.path.isdir(final_dir):
                         self.copy_files(final_dir, self.sadness)
-                    elif "surprise" in x and os.path.isdir(final_dir):
+                    elif self.surprise in x and os.path.isdir(final_dir):
                         self.copy_files(final_dir, self.surprise)
                     else:
                         continue
 
-    def copy_files(self, final_dir, anger):
-        for pic in os.listdir(final_dir):
+    def copy_files(self, final_dir, emotion, percent=0.2):
+        # Speed up training just use less data
+        full_dir = os.listdir(final_dir)
+        random.shuffle(full_dir)
+        length = int(len(full_dir)*percent)
+        list_dir = full_dir[0:length]
+
+        split_size = int(len(list_dir)*self.train_test_split)
+        train = list_dir[0:split_size]
+        test = list_dir[split_size:]
+
+        for pic in train:
             file_ = os.path.join(final_dir, pic)
-            dest_ = os.path.join(anger, pic)
+            emotion_dir = os.path.join(self.train, emotion)
+            dest_ = os.path.join(emotion_dir, pic)
             if os.path.getsize(file_) != 0:
-                print(os.getcwd())
                 copyfile(file_, dest_)
+
+        for pic in test:
+            file_ = os.path.join(final_dir, pic)
+            emotion_dir = os.path.join(self.test, emotion)
+            dest_ = os.path.join(emotion_dir, pic)
+            if os.path.getsize(file_) != 0:
+                copyfile(file_, dest_)
+
+    def delete_files(self, mypath):
+        for root, dirs, files in os.walk(mypath):
+            for x in files:
+                os.remove(os.path.join(root, x))
     
 if __name__ == "__main__":
     main()
