c702180d857b93b9bfc07cd5824242b0b6c9aacf
Erik Remington Lee <anathema8808@yahoo.com>
2020-06-09 00:01:12 -0400

    spagetti working model

--- face_detection.py
+++ face_detection.py
@@ -1,14 +1,36 @@
+import os
+import zipfile
+import random
+import shutil
+from shutil import copyfile
 import cv2.cv2 as cv2
+
+import tensorflow as tf
 import numpy as np
+import matplotlib as plt
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image
 
 
 def main():
-    Faces.find_face()
+    xyz = tf.keras.models.load_model("emotion_model.model")
+    test_dir = "emotions/test/"
+    test_datagen = ImageDataGenerator(rescale=1./255)
+    test_generator = test_datagen.flow_from_directory(test_dir, target_size=(256,256), 
+    batch_size=1, class_mode="categorical")
+    xyz.evaluate(test_generator)
+
+    Faces(xyz).find_face()
 
 class Faces:
+    def __init__(self, model):
+        self.model = model
 
-    @staticmethod
-    def find_face():
+    
+    def find_face(self):
         face_cascade = cv2.CascadeClassifier("hc.xml")
         path = "face_test/"
         cap = cv2.VideoCapture(0)
@@ -21,11 +43,13 @@ class Faces:
             for (x,y,w,h) in faces:
                 cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
                 face = gray[y:y+h, x:x+w]
-                txt = "face_"+str(count)+".jpg"
-                cv2.imwrite(path+txt, face)
-                count += 1
-                if count > 300:
-                    count = 1
+                print(self.predict(face))
+               
+                # txt = "face_"+str(count)+".jpg"
+                # cv2.imwrite(path+txt, face)
+                # count += 1
+                # if count > 300:
+                #     count = 1
 
             cv2.imshow("img", img)
             k = cv2.waitKey(30)
@@ -34,5 +58,21 @@ class Faces:
 
         cap.release()
         cv2.destroyAllWindows()
+    
+    
+
+    def predict(self, face):
+        size = 256
+        img_array = cv2.resize(face, (size,size))
+        # print(img_array.shape)
+        # image = np.expand_dims(img_array, axis=0)
+        # image = np.expand_dims(img_array, axis=-1)
+          
+        image = img_array.reshape(-1, size,size,1)
+        zeros = np.ones((1,1,1,3))
+        fi = image*zeros
+        acc = self.model.predict([fi])
+        print(acc)
+        return acc
 
 main()
