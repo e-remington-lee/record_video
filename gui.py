a19749209997471de49e8a683b7d51b1a992a935
Erik Remington Lee <anathema8808@yahoo.com>
2020-06-12 16:04:31 -0400

    refactored code

---
+++ gui.py
@@ -0,0 +1,8 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+def Application():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    window.setGeometry(x,y,w,h)
