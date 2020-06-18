90677e95b478ece1502dbb44852f563170096fdd
Erik Remington Lee <anathema8808@yahoo.com>
2020-06-18 01:42:01 -0400

    working on a buikd

--- gui.py
+++ gui.py
@@ -1,8 +1,32 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
+from face_detection_model import main, Faces
+
+
+def main_2():
+    Application()
 
 def Application():
     app = QApplication(sys.argv)
     window = QMainWindow()
-    window.setGeometry(x,y,w,h)
+    window.setGeometry(500,500,500,500)
+    window.setWindowTitle("ReLuu FaceNet")
+
+    label = QtWidgets.QLabel(window)
+    label.setText("Start Application")
+    label.move(200,50)
+
+    button = QtWidgets.QPushButton(window)
+    button.setText("Run Model")
+    button.move(200,100)
+    button.clicked.connect(main)
+
+    # button.setText("Stop Model")
+    # button.move(200,150)
+    # button.connect(Faces().stop())
+
+    window.show()
+    sys.exit(app.exec_())
+
+main_2()
