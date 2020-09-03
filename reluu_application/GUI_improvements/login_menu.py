d3e4add57a3b76a06218df601b5ddad90aaa9a8c
Erik Remington Lee <anathema8808@yahoo.com>
2020-09-03 02:16:17 -0400

    adding ui files

--- reluu_application/GUI_improvements/login_menu.py
+++ reluu_application/GUI_improvements/login_menu.py
@@ -2,30 +2,31 @@ import sys
 from PySide2.QtWidgets import QApplication, QSizePolicy, QVBoxLayout, QGridLayout, QWidget, QMainWindow, QLabel
 from PySide2.QtGui import QIcon, QPixmap
 from PySide2.QtCore import QRect, Qt
+from login_layout import Ui_MainWindow
 
-class LoginWindow(QMainWindow):
+class LoginWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super(LoginWindow, self).__init__(*args, **kwargs)
 
-        self.setWindowTitle("ReLuu")
-        self.layout = QGridLayout()
-        self.resize(470,378)
-        self.setStyleSheet("background-color: #f3fcff;")
-        self.show_logo()
-        self.setLayout(self.layout)
+        self.setupUi(self)
+    #     self.setWindowTitle("ReLuu")
+    #     self.layout = QGridLayout()
+    #     self.resize(470,378)
+    #     self.setStyleSheet("background-color: #f3fcff;")
+    #     self.show_logo()
+    #     self.setLayout(self.layout)
 
-    def show_logo(self):
-        logo = QLabel(self)
-        logo.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        logo.setAlignment(Qt.AlignCenter)
-        img = QPixmap("reluu_application\\GUI_improvements\\logo_transparent_background.png")
-        img2 = img.scaled(240,210)
-        logo.setPixmap(img2)
-        self.layout.addWidget(logo)
+    # def show_logo(self):
+    #     logo = QLabel(self)
+    #     logo.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+    #     logo.setAlignment(Qt.AlignCenter)
+    #     img = QPixmap("reluu_application\\GUI_improvements\\logo_transparent_background.png")
+    #     img2 = img.scaled(240,210)
+    #     logo.setPixmap(img2)
+    #     self.layout.addWidget(logo)
 
 
-        # logo.show()
-
+    #     # logo.show()
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- reluu_application/GUI_improvements/master_pyqt5/main_window.py
+++ reluu_application/GUI_improvements/master_pyqt5/main_window.py
@@ -13,14 +13,14 @@ class MainWindow(QtWidgets.QWidget):
         email_label = QtWidgets.QLabel("Email")
         password_label = QtWidgets.QLabel("Password")
 
-        email_input = QtWidgets.QLineEdit()
-        email_input.setPlaceholderText("Enter email here")
-        password_input = QtWidgets.QLineEdit()
-        password_input.setPlaceholderText("Enter password here")
-        password_input.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.email_input = QtWidgets.QLineEdit()
+        self.email_input.setPlaceholderText("Enter email here")
+        self.password_input = QtWidgets.QLineEdit()
+        self.password_input.setPlaceholderText("Enter password here")
+        self.password_input.setEchoMode(QtWidgets.QLineEdit.Password)
 
-        cancel_button = QtWidgets.QPushButton("Canel")
-        submit_button = QtWidgets.QPushButton("Login")
+        self.cancel_button = QtWidgets.QPushButton("Canel")
+        self.submit_button = QtWidgets.QPushButton("Login")
 
         # self.layout = QtWidgets.QVBoxLayout()
         # self.layout = QtWidgets.QGridLayout()
@@ -30,17 +30,39 @@ class MainWindow(QtWidgets.QWidget):
         # self.layout.addWidget(password_input, 3, 0)
 
         self.layout = QtWidgets.QFormLayout()
-        self.layout.addRow("Email", email_input)
-        self.layout.addRow("Password", password_input)
+        self.layout.addRow("Email", self.email_input)
+        self.layout.addRow("Password", self.password_input)
 
         abl = QtWidgets.QWidget()
         abl.setLayout(QtWidgets.QHBoxLayout())
-        abl.layout().addWidget(cancel_button)
-        abl.layout().addWidget(submit_button)
+        abl.layout().addWidget(self.cancel_button)
+        abl.layout().addWidget(self.submit_button)
         # self.layout.addLayout(action_button_layout)
         self.layout.addRow("", abl)
         self.setLayout(self.layout)
 
+        self.cancel_button.clicked.connect(self.close)
+        self.submit_button.clicked.connect(self.authenticate)
+
+        self.email_input.textChanged.connect(self.set_button_text)
+
+
+    def set_button_text(self, text): 
+        if text:
+            self.submit_button.setText(f'Login {text}')
+        else:
+            self.submit_button.setText(f'Login')
+
+
+    def authenticate(self):
+        em = "1234"
+        pw = "1234"
+
+        print(self.email_input, self.password_input)
+        if self.email_input.text() == em and self.password_input.text() == pw:
+            QtWidgets.QMessageBox.information(self, "Success!", "win!")
+        else:
+            QtWidgets.QMessageBox.critical(self, "Error", "login failed")
         
 
 if __name__ == '__main__':
