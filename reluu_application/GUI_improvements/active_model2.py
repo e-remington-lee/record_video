6e58b0208b05f9eafbf9ee790efbc8489b675cb0
Erik Remington Lee <anathema8808@yahoo.com>
2020-09-20 17:55:42 -0400

    added graphs to active model

---
+++ reluu_application/GUI_improvements/active_model2.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'active_model.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QMetaObject,
+    QObject, QPoint, QRect, QSize, QTime, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter,
+    QPixmap, QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_active_model(object):
+    def setupUi(self, active_model):
+        if not active_model.objectName():
+            active_model.setObjectName(u"active_model")
+        active_model.resize(300, 510)
+        palette = QPalette()
+        brush = QBrush(QColor(255, 255, 255, 255))
+        brush.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Base, brush)
+        palette.setBrush(QPalette.Active, QPalette.Window, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.Base, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.Window, brush)
+        palette.setBrush(QPalette.Disabled, QPalette.Base, brush)
+        palette.setBrush(QPalette.Disabled, QPalette.Window, brush)
+        active_model.setPalette(palette)
+        self.verticalLayout_2 = QVBoxLayout(active_model)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.active_model_layout = QVBoxLayout()
+        self.active_model_layout.setObjectName(u"active_model_layout")
+        self.icon_layout = QVBoxLayout()
+        self.icon_layout.setObjectName(u"icon_layout")
+        self.emotion_icon_layout = QVBoxLayout()
+        self.emotion_icon_layout.setObjectName(u"emotion_icon_layout")
+        self.emotion_icon_layout.setSizeConstraint(QLayout.SetFixedSize)
+
+        self.icon_layout.addLayout(self.emotion_icon_layout)
+
+        self.warning_layout = QVBoxLayout()
+        self.warning_layout.setSpacing(0)
+        self.warning_layout.setObjectName(u"warning_layout")
+        self.warning_layout.setSizeConstraint(QLayout.SetMaximumSize)
+        self.warning_layout.setContentsMargins(-1, 7, -1, 0)
+        self.warning_label1 = QLabel(active_model)
+        self.warning_label1.setObjectName(u"warning_label1")
+        self.warning_label1.setMaximumSize(QSize(166666, 15))
+        palette1 = QPalette()
+        brush1 = QBrush(QColor(214, 0, 0, 255))
+        brush1.setStyle(Qt.SolidPattern)
+        palette1.setBrush(QPalette.Active, QPalette.WindowText, brush1)
+        brush2 = QBrush(QColor(218, 0, 0, 255))
+        brush2.setStyle(Qt.SolidPattern)
+        palette1.setBrush(QPalette.Active, QPalette.Text, brush2)
+        brush3 = QBrush(QColor(255, 0, 0, 255))
+        brush3.setStyle(Qt.SolidPattern)
+        palette1.setBrush(QPalette.Active, QPalette.ToolTipText, brush3)
+        palette1.setBrush(QPalette.Inactive, QPalette.WindowText, brush1)
+        palette1.setBrush(QPalette.Inactive, QPalette.Text, brush2)
+        palette1.setBrush(QPalette.Inactive, QPalette.ToolTipText, brush3)
+        brush4 = QBrush(QColor(120, 120, 120, 255))
+        brush4.setStyle(Qt.SolidPattern)
+        palette1.setBrush(QPalette.Disabled, QPalette.WindowText, brush4)
+        palette1.setBrush(QPalette.Disabled, QPalette.Text, brush4)
+        palette1.setBrush(QPalette.Disabled, QPalette.ToolTipText, brush3)
+        self.warning_label1.setPalette(palette1)
+        font = QFont()
+        font.setFamily(u"Segoe UI Symbol")
+        font.setPointSize(8)
+        font.setBold(True)
+        font.setWeight(75)
+        self.warning_label1.setFont(font)
+        self.warning_label1.setScaledContents(False)
+        self.warning_label1.setAlignment(Qt.AlignCenter)
+        self.warning_label1.setWordWrap(False)
+
+        self.warning_layout.addWidget(self.warning_label1)
+
+        self.warning_label2 = QLabel(active_model)
+        self.warning_label2.setObjectName(u"warning_label2")
+        self.warning_label2.setMaximumSize(QSize(166666, 21))
+        palette2 = QPalette()
+        palette2.setBrush(QPalette.Active, QPalette.WindowText, brush1)
+        palette2.setBrush(QPalette.Inactive, QPalette.WindowText, brush1)
+        palette2.setBrush(QPalette.Disabled, QPalette.WindowText, brush4)
+        self.warning_label2.setPalette(palette2)
+        font1 = QFont()
+        font1.setFamily(u"Segoe UI Symbol")
+        self.warning_label2.setFont(font1)
+        self.warning_label2.setAlignment(Qt.AlignCenter)
+
+        self.warning_layout.addWidget(self.warning_label2)
+
+
+        self.icon_layout.addLayout(self.warning_layout)
+
+        self.detailed_view_layout = QVBoxLayout()
+        self.detailed_view_layout.setObjectName(u"detailed_view_layout")
+        self.detailed_view_layout.setSizeConstraint(QLayout.SetMaximumSize)
+        self.detailed_view_checkbox = QCheckBox(active_model)
+        self.detailed_view_checkbox.setObjectName(u"detailed_view_checkbox")
+        self.detailed_view_checkbox.setFont(font1)
+        self.detailed_view_checkbox.setChecked(False)
+
+        self.detailed_view_layout.addWidget(self.detailed_view_checkbox, 0, Qt.AlignHCenter)
+
+
+        self.icon_layout.addLayout(self.detailed_view_layout)
+
+
+        self.active_model_layout.addLayout(self.icon_layout)
+
+        self.button_layout = QHBoxLayout()
+        self.button_layout.setObjectName(u"button_layout")
+        self.button_layout.setContentsMargins(50, 7, 50, -1)
+        self.stop_session_button = QPushButton(active_model)
+        self.stop_session_button.setObjectName(u"stop_session_button")
+        font2 = QFont()
+        font2.setPointSize(10)
+        font2.setBold(True)
+        font2.setWeight(75)
+        self.stop_session_button.setFont(font2)
+
+        self.button_layout.addWidget(self.stop_session_button)
+
+
+        self.active_model_layout.addLayout(self.button_layout)
+
+
+        self.verticalLayout_2.addLayout(self.active_model_layout)
+
+
+        self.retranslateUi(active_model)
+
+        QMetaObject.connectSlotsByName(active_model)
+    # setupUi
+
+    def retranslateUi(self, active_model):
+        active_model.setWindowTitle(QCoreApplication.translate("active_model", u"Form", None))
+        self.warning_label1.setText(QCoreApplication.translate("active_model", u"No Face Detected", None))
+        self.warning_label2.setText(QCoreApplication.translate("active_model", u"Resize or Move the Face Box", None))
+        self.detailed_view_checkbox.setText(QCoreApplication.translate("active_model", u"Show Detailed View", None))
+        self.stop_session_button.setText(QCoreApplication.translate("active_model", u"Stop", None))
+    # retranslateUi
+
--- reluu_application/GUI_improvements/working_color_icon.py
+++ reluu_application/GUI_improvements/working_color_icon.py
@@ -9,7 +9,7 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
-from active_model import Ui_active_model
+from active_model2 import Ui_active_model
 
 # how to reuse embedded matplots
 # https://stackoverflow.com/questions/53258160/update-an-embedded-matplotlib-plot-in-a-pyqt5-gui-with-toolbar
@@ -17,26 +17,68 @@ from active_model import Ui_active_model
 # plot a color map
 # https://stackoverflow.com/questions/16834861/create-own-colormap-using-matplotlib-and-plot-color-scale
 
-class MplCanvas(FigureCanvasQTAgg):
+class EmotionIcon(FigureCanvasQTAgg):
     def __init__(self, parent=None, width=1, height=1, dpi=20):
         self.fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = self.fig.add_subplot(111)
-        super(MplCanvas, self).__init__(self.fig)
+        super(EmotionIcon, self).__init__(self.fig)
         self.setParent(parent)
         self.norm=plt.Normalize(-1,1)
-        self.cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["red","white","green"])
-        
+        self.cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["red","aliceblue","green"])
 
 
     def plot(self):
         # ax = self.fig.add_subplot(111)
+        c = np.random.rand(1,1)*2-1
         self.axes.cla()
-        x,y,c = zip(*np.random.rand(1,3)*2-1)
         self.axes.axis("off")        
-        self.axes.scatter(x,y,c=c, s=5000, cmap=self.cmap, norm=self.norm)
+        self.axes.scatter(1,1,c=c, s=5000, cmap=self.cmap, norm=self.norm)
         self.fig.canvas.draw_idle()
         # plt.colorbar()
         # plt.show()
+    
+    def invisible(self):
+        pass
+
+
+class DetailedView(FigureCanvasQTAgg):
+    def __init__(self, parent=None, width=1, height=1, dpi=20):
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        self.fig.subplots_adjust(left=0.3)
+        self.axes = self.fig.add_subplot(111)
+        
+        self.emotions = ["Anger", "Happy", "Neutral", "Saddness", "Surprise"]
+        self.axes.set_visible(False)
+        
+        # Once I figure out how to add data to axis...
+        # self.axes.get_xaxis().set_visible(False)
+
+        super(DetailedView, self).__init__(self.fig)
+        self.setParent(parent)
+
+
+    def plot(self):
+        self.axes.cla()      
+        index = [0,1,2,3,4]
+        data = [0.10, 0.7, 0.0, 0.0, .20]
+        self.axes.barh(self.emotions, data, color="darkturquoise")
+        self.axes.set_title("Emotion")
+        self.axes.set_xlim([0,1])
+        # for i, v in enumerate(data):
+        #     self.axes.text(i, str(v))
+        self.fig.canvas.draw_idle()
+        
+
+    def visible(self):
+        self.axes.set_visible(True)
+        self.fig.canvas.draw_idle()
+
+
+    def invisible(self):
+        self.axes.set_visible(False)
+        self.fig.canvas.draw_idle()
+        # self.figure.close()
+        # https://stackoverflow.com/questions/8213522/when-to-use-cla-clf-or-close-for-clearing-a-plot-in-matplotlib
 
 
 class MainWindow(QtWidgets.QWidget, Ui_active_model):
@@ -45,103 +87,39 @@ class MainWindow(QtWidgets.QWidget, Ui_active_model):
         self.setupUi(self)
         icon = QtGui.QIcon("reluu_application/GUI_improvements/logo_transparent_background.png")
         self.setWindowIcon(icon)
-        self.abc = False
-        # import os
-        # print(os.listdir())
 
-        self.sc = MplCanvas(self, width=5, height=4, dpi=100)
-        self.sc.plot()
-        self.emotion_icon_layout.addWidget(self.sc) 
-        # self.emotion_icon_layout.setHidden(True)
+        self.resize(300, 520)
+        self.detailed_view = DetailedView(self, width=1, height=2, dpi=100)
+        self.detailed_view.plot()
+        self.detailed_view_layout.addWidget(self.detailed_view)
+        # self.detailed_view.axes.set_visible(False)
 
-        # button1 = QtWidgets.QPushButton("push", clicked=self.idk)
-        # self.layout.addWidget(button1)
-        # self.setLayout(self.layout)
-        self.show()
+        self.error = False
+
+        self.ei = EmotionIcon(self, width=1, height=1, dpi=100)
+        self.ei.plot()
+        self.emotion_icon_layout.addWidget(self.ei)        
+
         self.stop_session_button.clicked.connect(self.idk)
         self.detailed_view_checkbox.stateChanged.connect(self.show_details)
+        self.show()
     
     def show_details(self):
         if self.detailed_view_checkbox.isChecked():
-            self.resize(248, 450)
+            self.detailed_view.visible()
         else:
-            self.resize(248, 304)
+            # self.resize(300, 320)
+            self.detailed_view.invisible()
 
 
     def idk(self):
-        self.show_details()
-        self.sc.plot()
-        self.abc = not self.abc
-        self.warning_label1.setHidden(self.abc)
-        self.warning_label2.setHidden(self.abc)
+        self.ei.plot()
+        self.error = not self.error
+        self.warning_label1.setHidden(self.error)
+        self.warning_label2.setHidden(self.error)
 
 
-app = QtWidgets.QApplication(sys.argv)
-w = MainWindow()
-app.exec_()
-# import sys
-
-# from PySide2.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QPushButton
-# from PySide2.QtGui import QIcon
-
-# import matplotlib
-# import numpy as np
-# from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-# from matplotlib.figure import Figure
-# import matplotlib.pyplot as plt
-
-# import random
-
-# class App(QMainWindow):
-
-#     def __init__(self):
-#         super().__init__()
-#         self.left = 100
-#         self.top = 100
-#         self.title = 'PyQt5 matplotlib example - pythonspot.com'
-#         self.width = 640
-#         self.height = 400
-#         self.initUI()
-
-#     def initUI(self):
-#         self.setWindowTitle(self.title)
-#         self.setGeometry(self.left, self.top, self.width, self.height)
-
-#         m = PlotCanvas(self, width=5, height=4)
-#         m.move(0,0)
-
-#         button = QPushButton('PyQt5 button', self)
-#         button.setToolTip('This s an example button')
-#         button.move(500,0)
-#         button.resize(140,100)
-
-#         self.show()
-
-
-# class PlotCanvas(FigureCanvas):
-
-#     def __init__(self, parent=None, width=5, height=4, dpi=100):
-#         fig = Figure(figsize=(width, height), dpi=dpi)
-#         self.axes = fig.add_subplot(111)
-
-#         FigureCanvas.__init__(self, fig)
-#         self.setParent(parent)
-
-#         FigureCanvas.setSizePolicy(self,
-#                 QSizePolicy.Expanding,
-#                 QSizePolicy.Expanding)
-#         FigureCanvas.updateGeometry(self)
-#         self.plot()
-
-
-#     def plot(self):
-#         data = [random.random() for i in range(25)]
-#         ax = self.figure.add_subplot(111)
-#         ax.axis("off")
-#         ax.plot(data, 'r-')
-#         self.draw()
-
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
-#     ex = App()
-#     sys.exit(app.exec_())
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    app.exec_()
